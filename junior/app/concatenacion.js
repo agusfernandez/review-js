c602bd59ff754b47e6d85e668c1eaff2b3dace94
agusfernandez <agustina.fer91@gmail.com>
2022-05-03 00:35:13 -0300

    concatenacion

---
+++ junior/app/concatenacion.js
@@ -0,0 +1,30 @@
+let saludo = "Hola";
+let nombre = "Agustina";    
+console.log("esto es una union de " + " " + saludo + " " + nombre);
+let respuesta = saludo + " " + nombre
+//document.write (respuesta);
+
+
+let numero1=56;
+let numero2=69;
+//aca suma los numeros como cuenta
+let respuesta2= numero1 + numero2;
+console.log(respuesta2);
+// aca puedo fozar el string y no suma los numeros
+let respuesta3= " " + numero1 + numero2;
+console.log(respuesta3);
+
+// CONCAT _ es una funcion exclusiva para strings
+// .concat
+let hola= "hola";
+let chau= "chau";
+let frase= hola.concat(chau);
+console.log(frase);
+
+
+// Clave esttto 
+let user = "agustina";
+let bienvenida = `hola ${user}`;
+console.log(bienvenida);
+
+
